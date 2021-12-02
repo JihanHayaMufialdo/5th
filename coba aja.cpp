30e80548d0b0e3eddffdf36f4da7e9c9f5c6a74f
Jihan Haya Mufialdo <92241700+JihanHayaMufialdo@users.noreply.github.com>
2021-12-02 23:20:37 +0700

    Update coba aja.cpp

--- coba aja.cpp
+++ coba aja.cpp
@@ -1,58 +1,8 @@
 #include <iostream>
-//#include <ncurses\ncurses.h>
 using namespace std;
 
 int main(){
 
-//initscr();
-
-	string arr[18][32];
-	int x,y;
-	
-	//STORAGE OF THE MAP
-	
-	for( x=0; x<18; x++){
-		for(y=0; y<32; y++){
-			cout << arr[x][y];
-		}
-	}
- 
- 	//DESIGNING THE MAP
- 	
- 	for(x=0; x<18; x++){
-		for(y=0; y<32; y++){	
-		
-			if(x==0||x==32){
-				arr[x][y] = "#";
-			}
-			
-			else if(y==0||y==32){
-				arr[x][y] = "|";
-			}
-			
-			else if(x==5||x==9){
-				arr[x][y] = "#";
-			}
-			
-//			else{
-//				
-//				arr[x][y] = ".";
-//			}
-		}
- 	}
- 	
- 	//PRINTING THE MAP
- 	
- 	for(x=0; x<15; x++){
-		for(y=0; y<15; y++){
-			cout << arr[x][y] << " ";
-		}
-			cout << endl;
-	}
-	
-//	refresh();
-//	getch();
-//	endwin();
 }
 	
 
